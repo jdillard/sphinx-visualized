17c50cb92b973e185ed424407d0fafbd737a8e5e
Jared Dillard <jared.dillard@gmail.com>
2024-10-24 16:57:45 -0700

    Make variable names more unique

--- sphinx_visualised/__init__.py
+++ sphinx_visualised/__init__.py
@@ -146,11 +146,11 @@ def create_json(app, exception):
     """
     filename = Path(app.outdir) / "_static" / "links.js"
     with open(filename, "w") as json_file:
-        json_file.write(f'var links = {json.dumps(app.env.app.links, indent=4)};')
+        json_file.write(f'var links_data = {json.dumps(app.env.app.links, indent=4)};')
 
     filename = Path(app.outdir) / "_static" / "nodes.js"
     with open(filename, "w") as json_file:
-        json_file.write(f'var nodes = {json.dumps(app.env.app.nodes, indent=4)};')
+        json_file.write(f'var nodes_data = {json.dumps(app.env.app.nodes, indent=4)};')
 
     filename = Path(app.outdir) / "_static" / "toctree.js"
     with open(filename, "w") as json_file:
